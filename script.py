e268c459c9da92271313f9951b3bca27c805b04c
Aleksandre <55573405+Aleksandre1337@users.noreply.github.com>
2023-12-27 16:08:43 +0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+# We have hidden code in another file. Inspect the simulate.py file to see how it works
+
+from simulate import simulate
+
+num_people_in_room = 30 			#Change This Number (keep it smaller than 100 to save processing power)
+
+simulate(num_people_in_room)
